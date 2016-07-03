06e152ed4df2c714c9c4f76febc8328efea3363c
Jian-Hua, He <game09116@gmail.com>
2016-07-03 12:26:14 +0800

    Add font detect in content script

--- resources/js/content.js
+++ resources/js/content.js
@@ -1,1 +1,14 @@
-console.log('content');
+import $ from 'jquery';
+import { detectFont, supportedFonts } from 'detect-font';
+
+$(function () {
+
+    let chrome = window.chrome;
+
+    $('h1, h2, h3, p, a, span').mouseenter(function (e) {
+        let that = this;
+        let fontFamily = detectFont(that);
+        console.log(fontFamily);
+    });
+
+});
