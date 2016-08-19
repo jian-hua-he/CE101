2b8464bd99fbe10f62211dc143e0a05e18bfbd57
Jian-Hua, He <game09116@gmail.com>
2016-08-19 11:32:49 +0800

    Make variable more readable

--- resources/js/eventPage.js
+++ resources/js/eventPage.js
@@ -3,11 +3,11 @@ import $ from 'jquery';
 $(() => {
 
     let chrome = window.chrome;
-    let active = false;
+    let currentStatus = false;
 
     chrome.browserAction.onClicked.addListener((tab) => {
-        chrome.tabs.sendMessage(tab.id, {active: active}, (response) => {
-            active = !active;
+        chrome.tabs.sendMessage(tab.id, {active: !currentStatus}, (response) => {
+            currentStatus = !currentStatus;
         });
     });
 });
