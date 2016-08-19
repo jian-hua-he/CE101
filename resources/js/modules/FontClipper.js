87d072e575c535fe19ace174e300d9cf2823db6c
Jian-Hua, He <game09116@gmail.com>
2016-08-19 17:45:57 +0800

    Import Tooltip into FontClipper class

--- resources/js/modules/FontClipper.js
+++ resources/js/modules/FontClipper.js
@@ -1,30 +1,38 @@
 import $ from 'jquery';
 import { detectFont, supportedFonts } from 'detect-font';
+import Tooltip from './Tooltip';
 
-let _bindEvent = (selector) => {
-    selector.on('mouseenter', (e) => { 
-        let fontFamily = detectFont(e.target); 
-        console.log(fontFamily); 
+let _bindEvent = (selector, tooltip) => {
+    selector.on('mouseenter', (e) => {
+        let fontFamily = detectFont(e.target);
+        tooltip.setText(fontFamily);
+        tooltip.display();
+    });
+
+    selector.on('mouseleave', (e) => {
+        tooltip.hide();
     });
 };
 
-let _unbindEvent = (selector) => {
-    selector.on('mouseenter');
+let _unbindEvent = (selector, tooltip) => {
+    selector.unbind('mouseenter')
+    selector.unbind('mouseleave');
 };
 
 class FontClipper {
     constructor() {
         this.selector = $('h1, h2, h3, h4, h5, h6, p, a, span');
+        this.tooltip = new Tooltip;
     }
 
     enable() {
         console.log('Enable Font Clipper');
-        _bindEvent(this.selector);
+        _bindEvent(this.selector, this.tooltip);
     }
 
     disable() {
         console.log('Disable Font Clipper');
-        _unbindEvent(this.selector);
+        _unbindEvent(this.selector, this.tooltip);
     }
 }
 
