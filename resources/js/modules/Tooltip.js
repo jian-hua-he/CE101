da45e78c05a1be4b8c78f6defe471cd6569186b0
Jian-Hua, He <game09116@gmail.com>
2016-08-19 17:46:12 +0800

    Create Tooltip class

---
+++ resources/js/modules/Tooltip.js
@@ -0,0 +1,20 @@
+ class Tooltip {
+    constructor() {
+        this.text = '';
+    }
+
+    setText(text) {
+        this.text = text;
+        console.log('Set text: ' + this.text);
+    }
+
+    display() {
+        console.log('Display tooltip');
+    }
+
+    hide() {
+        console.log('Hide tooltip');
+    }
+ }
+
+ export default Tooltip;
