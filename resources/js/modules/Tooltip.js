91556ef74aab44025bb5a583d3d37027e0e8f164
Jian-Hua, He <game09116@gmail.com>
2016-08-30 23:56:36 +0800

    Implement setStyle

--- resources/js/modules/Tooltip.js
+++ resources/js/modules/Tooltip.js
@@ -2,29 +2,35 @@ import $ from 'jquery'
 
 class Tooltip {
     constructor() {
-        let body;
-
-        if (!body) {
-            body = $('body');
-        }
-
-        body.append("<div class='font-clipper-tooltip'></div>");
-
-        this.tooltip = $('.font-clipper-tooltip');
+        $('body').append("<div id='font-clipper-tooltip'></div>");
+        this.el = $('#font-clipper-tooltip');
     }
 
     setText(text) {
-        this.tooltip.text(text);
+        this.el.text(`Font Face: ${text}`);
         console.log('Set text: ' + text);
     }
 
+    setStyle(top, left) {
+        this.el.css({
+            'font-size': '12px',
+            'color': '#DDD',
+            'background': '#333',
+            'position': 'absolute',
+            'padding': '0.5em',
+            'border-radius': '0.5em',
+            'top': top,
+            'left': left
+        });
+    }
+
     display() {
-        this.tooltip.show();
+        this.el.show();
         console.log('Display tooltip');
     }
 
     hide() {
-        this.tooltip.hide();
+        this.el.hide();
         console.log('Hide tooltip');
     }
 }
