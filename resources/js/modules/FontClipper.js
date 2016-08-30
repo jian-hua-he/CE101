5ad576ebfef9fbfabb172362a42a871e4317d4c8
Jian-Hua, He <game09116@gmail.com>
2016-08-30 23:56:04 +0800

    Add tooltip init process in mouseenter event

--- resources/js/modules/FontClipper.js
+++ resources/js/modules/FontClipper.js
@@ -5,7 +5,12 @@ import Tooltip from './Tooltip';
 let _bindEvent = (selector, tooltip) => {
     selector.on('mouseenter', (e) => {
         let fontFamily = detectFont(e.target);
+        let target = $(e.target);
+        let top = target.offset().top;
+        let left = target.offset().left;
+
         tooltip.setText(fontFamily);
+        tooltip.setStyle(top, left);
         tooltip.display();
     });
 
@@ -15,24 +20,24 @@ let _bindEvent = (selector, tooltip) => {
 };
 
 let _unbindEvent = (selector, tooltip) => {
-    selector.unbind('mouseenter')
+    selector.unbind('mouseenter');
     selector.unbind('mouseleave');
 };
 
 class FontClipper {
     constructor() {
-        this.selector = $('h1, h2, h3, h4, h5, h6, p, a, span');
+        this.el = $('h1, h2, h3, h4, h5, h6, p, a, span, ul, ol, li');
         this.tooltip = new Tooltip;
     }
 
     enable() {
         console.log('Enable Font Clipper');
-        _bindEvent(this.selector, this.tooltip);
+        _bindEvent(this.el, this.tooltip);
     }
 
     disable() {
         console.log('Disable Font Clipper');
-        _unbindEvent(this.selector, this.tooltip);
+        _unbindEvent(this.el, this.tooltip);
     }
 }
 
